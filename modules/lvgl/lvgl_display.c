21f5308db606841dcbfe07e859567c8f42ea980c
Grzegorz Runc <g.runc@grinn-global.com>
2024-12-04 09:09:46 +0100

    modules: lvgl_display: use DMA2D to transfer buffer to fb
    Use DMA2D to transfer LVGL buffer to LTDC framebuffer.
    It allows to offload data transfer and free CPU, as function can
    return and interrupt is used to determine if transfer was completed.
    It also offers significant performance gains due to direct and
    fast access to both internal and external RAM memories.

--- modules/lvgl/lvgl_display.c
+++ modules/lvgl/lvgl_display.c
@@ -8,6 +8,13 @@
 #include <zephyr/kernel.h>
 #include <errno.h>
 
+#ifdef CONFIG_LV_Z_USE_DMA2D_FOR_FRAMEBUFFER
+
+#include <zephyr/irq.h>
+#include <stm32_ll_dma2d.h>
+
+#endif /* CONFIG_LV_Z_USE_DMA2D_FOR_FRAMEBUFFER */
+
 #include "lvgl_display.h"
 
 #ifdef CONFIG_LV_Z_FLUSH_THREAD
@@ -16,20 +23,62 @@ K_SEM_DEFINE(flush_complete, 0, 1);
 /* Message queue will only ever need to queue one message */
 K_MSGQ_DEFINE(flush_queue, sizeof(struct lvgl_display_flush), 1, 1);
 
+#ifdef CONFIG_LV_Z_USE_DMA2D_FOR_FRAMEBUFFER
+#define DISPLAY_NODE DT_CHOSEN(zephyr_display)
+
+#define DISPLAY_WIDTH  DT_PROP(DISPLAY_NODE, width)
+#define DISPLAY_HEIGHT DT_PROP(DISPLAY_NODE, height)
+
+static lv_disp_drv_t *lvgl_disp_driver;
+
+static void stm32_dma2d_irq_handler(const struct device *dev)
+{
+	LL_DMA2D_DisableIT_TC(DMA2D);
+	LL_DMA2D_ClearFlag_TC(DMA2D);
+
+	lv_disp_flush_ready(lvgl_disp_driver);
+	k_sem_give(&flush_complete);
+}
+
+#endif /* CONFIG_LV_Z_USE_DMA2D_FOR_FRAMEBUFFER */
+
 void lvgl_flush_thread_entry(void *arg1, void *arg2, void *arg3)
 {
 	struct lvgl_display_flush flush;
 	struct lvgl_disp_data *data;
 
+#ifdef CONFIG_LV_Z_USE_DMA2D_FOR_FRAMEBUFFER
+	uint32_t framebuffer_addr = (uint32_t)display_get_framebuffer(DEVICE_DT_GET(DISPLAY_NODE));
+
+	IRQ_CONNECT(DMA2D_IRQn, 5, stm32_dma2d_irq_handler, NULL, 0);
+	irq_enable(DMA2D_IRQn);
+#endif /* CONFIG_LV_Z_USE_DMA2D_FOR_FRAMEBUFFER */
+
 	while (1) {
 		k_msgq_get(&flush_queue, &flush, K_FOREVER);
 		data = (struct lvgl_disp_data *)flush.disp_drv->user_data;
 
+#ifdef CONFIG_LV_Z_USE_DMA2D_FOR_FRAMEBUFFER
+		LL_DMA2D_SetMode(DMA2D, LL_DMA2D_MODE_M2M);
+		LL_DMA2D_FGND_SetColorMode(DMA2D, LL_DMA2D_INPUT_MODE_ARGB8888);
+		LL_DMA2D_FGND_SetMemAddr(DMA2D, (uint32_t)flush.buf);
+		LL_DMA2D_FGND_SetLineOffset(DMA2D, 0);
+		LL_DMA2D_SetOutputColorMode(DMA2D, LL_DMA2D_OUTPUT_MODE_ARGB8888);
+		LL_DMA2D_SetOutputMemAddr(DMA2D,
+				framebuffer_addr + 4 * (flush.y * DISPLAY_WIDTH + flush.x));
+		LL_DMA2D_SetLineOffset(DMA2D, DISPLAY_WIDTH - flush.desc.width);
+		LL_DMA2D_SetNbrOfPixelsPerLines(DMA2D, flush.desc.width);
+		LL_DMA2D_SetNbrOfLines(DMA2D, flush.desc.height);
+		LL_DMA2D_ClearAllFlags(DMA2D);
+		LL_DMA2D_EnableIT_TC(DMA2D);
+		LL_DMA2D_Start(DMA2D);
+#else
 		display_write(data->display_dev, flush.x, flush.y, &flush.desc,
 			      flush.buf);
 
 		lv_disp_flush_ready(flush.disp_drv);
 		k_sem_give(&flush_complete);
+#endif /* CONFIG_LV_Z_USE_DMA2D_FOR_FRAMEBUFFER */
 	}
 }
 
@@ -73,6 +122,10 @@ int set_lvgl_rendering_cb(lv_disp_drv_t *disp_drv)
 #ifdef CONFIG_LV_Z_FLUSH_THREAD
 	disp_drv->wait_cb = lvgl_wait_cb;
 #endif
+
+#ifdef CONFIG_LV_Z_USE_DMA2D_FOR_FRAMEBUFFER
+	lvgl_disp_driver = disp_drv;
+#endif /* CONFIG_LV_Z_USE_DMA2D_FOR_FRAMEBUFFER */
 
 	switch (data->cap.current_pixel_format) {
 	case PIXEL_FORMAT_ARGB_8888:
